e4e6112572bdb6b47623e571f00e9c09f40f473f
morozovcookie <morozov.cookie@gmail.com>
2016-01-22 03:55:45 +0300

    Initial Import

--- admin/js/app.js
+++
@@ -1,3 +0,0 @@
-var app = angular.module('main', ['ngCookies']);
-var auth = angular.module('auth', ['ngCookies']);
-var admin = angular.module('admin', ['ngRoute','ngCookies']);
--- admin/js/controller/navbar-controller.js
+++
@@ -1,9 +0,0 @@
-admin.controller('navbarController', function($scope, $cookieStore, $window){
-	if ($window.localStorage.getItem('user')==undefined)
-		$window.location.href = serv_addr + 'admin/index.html';
-
-	$scope.exit = function(){
-		$window.localStorage.removeItem('user');
-		$window.location.href = serv_addr + 'admin/index.html';
-	}
-});
