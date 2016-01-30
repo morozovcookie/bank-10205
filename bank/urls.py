a702cc0d0724bf97f8374af691922f9c6b1be83d
Intey <ziexe0@gmail.com>
2016-01-30 21:38:44 +0300

    [endlines]

--- bank/urls.py
+++ bank/urls.py
@@ -4,14 +4,14 @@ from banking import views
 from banking.api import user as api_user
 
 urlpatterns = [
-    #paged routing
+    # paged routing
     url(r'^$', views.default),
     url(r'^auth/', views.auth),
     url(r'^client/', views.client),
     url(r'^admin/', views.admin),
     url(r'^error/', views.error),
 
-    #api calls
+    # api calls
     url(r'^api/auth/', api_user.auth.as_view(), name='auth'),
     url(r'^api/user/', api_user.user.as_view(), name='user'),
     url(r'^api/users/', api_user.user_list, name='user-list'),
