b5919d2e8030b48d0b474725f76b15de5f63ac48
morozovcookie <morozov.cookie@gmail.com>
2016-01-30 16:50:09 +0300

    update user api

--- banking/api/user.py
+++ banking/api/user.py
@@ -7,15 +7,19 @@ from rest_framework import status
 
 from django.contrib.auth.models import User
 
-class TestView(APIView):
-    def get(self, request, format=None):
-        return Response({
-            'detail': "GET Response"
-        })
-        
+from banking.views import has_permisions
+from banking.serializers.user import UserSerializer
+
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from django.http import JsonResponse, HttpResponse
+
+class auth(APIView):
     def post(self, request, format=None):
+        print 'debug'
         try:
-            data = request.DATA
+            data = request.data
         except ParseError as error:
             return Response(
                 'Invalid JSON - {0}'.format(error.detail),
@@ -34,14 +38,29 @@ class TestView(APIView):
             )
         token = Token.objects.get_or_create(user=user)
         return Response({
-            'detail': 'POST answer',
             'token': token[0].key
         })
         
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-
-class AuthView(APIView):
+    def delete(self, request, format=None):
+        try:
+            key = request.META.get('HTTP_AUTHORIZATION').split()[1]
+        except ParseError as error:
+            return Response(
+                'Invalid HTTP request - {0}'.format(error.detail),
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        token = Token.objects.get(key=key)
+        if not token:
+            return Response(
+                'User was not authorized',
+                status=status.HTTP_404_NOT_FOUND
+            )
+        token.delete()
+        return Response({
+            'detail': 'Token has been deleted'
+        })
+        
+class user(APIView):
     authentication_classes = (
         TokenAuthentication,
     )
@@ -50,6 +69,45 @@ class AuthView(APIView):
     )
     
     def get(self, request, format=None):
+        print 'get'
+        key = request.META.get('HTTP_AUTHORIZATION')   
+        if key is None: 
+            return Response(
+                'Invalid HTTP request - {0}',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        key = key.split()[1]
+        user = User.objects.get(auth_token=key)
+        user = UserSerializer(user)
         return Response({
-            'detail': 'I suppose you are authenticated'
+            'user': user.data
+        })
+        
+    def post(self, request, format=None):
+        pass
+        
+class user_list(APIView):
+    authentication_classes = (
+        TokenAuthentication,
+    )
+    permission_classes = (
+        IsAuthenticated,
+    )
+    
+    def get(self, request, format=None):
+        try:
+            if not has_permisions(request):
+                return HttpResponse(
+                    'You do not have permission',
+                    status=status.HTTP_403_FORBIDDEN
+                )
+        except ParseError:
+            return HttpResponse(
+                'Invalid HTTP request - {0}',
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        users = User.objects.all()
+        users = UserSerializer(users, many=True)
+        return JsonResponse({
+            'users' : users.data
         })
