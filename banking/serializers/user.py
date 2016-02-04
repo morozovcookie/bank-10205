fa01b4c17b2fe6379bd4347b67f9924083a6f095
morozovcookie <morozov.cookie@gmail.com>
2016-02-04 14:37:40 +0300

    update user serializer: remove e-mail field from response

--- banking/serializers/user.py
+++ banking/serializers/user.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name', 'is_superuser')
+        fields = ('id', 'username', 'first_name', 'last_name', 'is_superuser')
