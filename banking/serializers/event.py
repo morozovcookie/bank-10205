0102722b7330bb04be3cd113c9ee27f4f9cdcbf3
Intey <ziexe0@gmail.com>
2016-02-08 01:32:12 +0300

    [fix] Create serializer for event with participants.

--- banking/serializers/event.py
+++ banking/serializers/event.py
@@ -1,14 +1,17 @@
 from rest_framework import serializers
 from banking.models import Event
-# from banking.serializers.user import AccountSerializer
+from banking.serializers.user import AccountSerializer
 
 
 class EventSerializer(serializers.ModelSerializer):
+    #  fix author field. Show its str representation(__str__ in account)
+    #  instead pk.
     author = serializers.StringRelatedField(many=False)
-    # participants = AccountSerializer(many=True, source='get_participants')
 
     class Meta:
         model = Event
-        field = (
-            'id', 'name', 'date', 'price', 'author',  # 'participants'
-        )
+
+class EventFullSerializer(EventSerializer):
+    # append list of participants, getted from `get_participants()` method of
+    # event.
+    participants = AccountSerializer(many=True, source='get_participants')
