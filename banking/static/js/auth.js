8c453933ddfa8af3baedd166fdc1f63d6cea9268
Intey <ziexe0@gmail.com>
2016-02-23 12:49:14 +0300

    [fix] b1c57f6 bad merge.

--- banking/static/js/auth.js
+++ banking/static/js/auth.js
@@ -1,22 +1,58 @@
+function getCookie(name) {
+	var cookieValue = null;
+	if (document.cookie && document.cookie != '') {
+		var cookies = document.cookie.split(';');
+		for (var i = 0; i < cookies.length; i++) {
+			var cookie = jQuery.trim(cookies[i]);
+			// Does this cookie string begin with the name we want?
+			if (cookie.substring(0, name.length + 1) == (name + '=')) {
+				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+				break;
+			}
+		}
+	}
+	return cookieValue;
+}
+function csrfSafeMethod(method) {
+	// these HTTP methods do not require CSRF protection
+	return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+function postSCRF(settings){
+	var csrftoken = getCookie('csrftoken');
+	settings.beforeSend = function(xhr, settings) {
+		if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+			xhr.setRequestHeader("X-CSRFToken", csrftoken);
+		}
+	};
+	settings.method = "POST";
+	return $.ajax(settings);
+}
 var AuthForm = React.createClass({
     getInitialState: function(){
         return {
             username: '',
-            password: ''
+            password: '',
+            csrf: ''
         }
     },
     handleUsernameChange: function(event){
         this.setState({
             username: event.target.value
-        });  
+        });
     },
     handlePasswordChange: function(event){
         this.setState({
             password: event.target.value
-        });  
+        });
     },
     handleAuth: function(){
-        $.post('/api/auth/', this.state)
+        var data = this.state;
+
+
+		postSCRF({
+			method:'POST',
+			url:'/api/auth/',
+			data:this.state})
         .success(function(response){
             var token = response.token;
             window.localStorage.setItem('token', token);
@@ -28,9 +64,16 @@ var AuthForm = React.createClass({
                 },
                 dataType: 'json',
                 success: function(response){
-                    window.localStorage.setItem('user', JSON.stringify(response));
-                    console.log(response.is_superuser);
-                    document.location.href = (response.is_superuser == true ? '/admin/' : '/client/');   
+                    if (response.user.is_superuser)
+                    {
+                        document.location.href = '/admin/';
+                        window.localStorage.setItem('is_superuser', 'true');
+                    }
+                    else
+                    {
+                        document.location.href = '/client/';
+                        window.localStorage.setItem('is_superuser', 'false');
+                    }
                 }
             });
         });
@@ -56,8 +99,8 @@ var Edit = React.createClass({
         return (
             <div className="input-group">
                 <span className="input-group-addon" id="username-label">{this.props.label}</span>
-                <input type={this.props.type} className="form-control" id="username-input" 
-                form="auth-form" aria-describedby="username-label" 
+                <input type={this.props.type} className="form-control" id="username-input"
+                form="auth-form" aria-describedby="username-label"
                 onChange={this.props.change}/>
             </div>
         );
@@ -68,7 +111,7 @@ var Button = React.createClass({
     render: function(){
         return (
             <div className="btn-group col-md-12">
-                <button type="button" className="btn btn-success col-md-12" 
+                <button type="button" className="btn btn-success col-md-12"
                 id="sigin-button" form="auth-form" onClick={this.props.click}>
                     {this.props.caption}
                 </button>
