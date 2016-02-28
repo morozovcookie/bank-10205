4633dc1f7e11df8b53e0025b7105bcca8132b99d
Intey <ziexe0@gmail.com>
2016-02-28 16:54:21 +0300

    [up|doc] Add docstrings for Event views.

--- banking/api/event.py
+++ banking/api/event.py
@@ -8,7 +8,8 @@ from banking.models import Event
 from banking.serializers.event import *
 
 
-def get_event(pk):
+def _get_event(pk):
+    """ Internal helper. Get event by pk or 404. """
     try:
         e = Event.objects.get(pk=pk)
         return e
@@ -16,8 +17,8 @@ def get_event(pk):
         raise Http404
 
 
-def get_participation(e, pk):
-    """Return participation dict.
+def _get_participation(e, pk):
+    """ Internal helper. Get participation dict, or 404.
     keys:
         account -- account object
         rate -- participation parts
@@ -36,40 +37,74 @@ def get_participation(e, pk):
 
 
 class EventListView(generics.ListCreateAPIView):
+    """ Show Event with participants list.  """
     model = Event
     serializer_class = EventFullSerializer
     queryset = Event.objects.all()
 
     def post(self, request):
+        """ Create new event. Accept event data and array of participation. """
         self.serializer_class = EventPostSerializer
         return super(EventListView, self).post(request)
 
+    def get_serializer_class(self, *args, **kwargs):
+        """ Correctly show Serializer for different requests.
+        In each req-type function, firstly changes the serializer. But swagger
+        use this method, to get serializer class. In default realization it's
+        just return self.serializer_class(EventFullSerializer)."""
+        if self.request.method == 'POST':
+            return EventPostSerializer
+        else:
+            return EventFullSerializer
+
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    """ Show concrete event data.
+    Similar for EventListView, but return single instance of Event.
+    """
     model = Event
     serializer_class = EventFullSerializer
     queryset = Event.objects.all()
 
     def put(self, request, pk):
+        """ Update event fields. Get all required event fields. """
         self.serializer_class = EventPostSerializer
         return super(EventDetail, self).put(request, pk)
 
     def patch(self, request, pk):
+        """ Update single event field. Good for single field changes. """
         self.serializer_class = EventPostSerializer
         return super(EventDetail, self).patch(request, pk)
 
+    # just for docs
+    def delete(self, req, pk):
+        """ [UTESTED] Remove event.
+        Delete all it's participations and transactions. """
+        return super(EventDetail, self).delete(req, pk)
+
+    def get_serializer_class(self, *args, **kwargs):
+        """ Correctly show Serializer for different requests. """
+        if self.request.method in ['PATCH', 'PUT']:
+            return EventPostSerializer
+        else:
+            return EventFullSerializer
+
 
 class ParticipantListView(views.APIView):
+    """
+    View for non model entity - 'participation-row' - account + parts.
+    Each participation determine how some account participate in some event.
+    """
     def get(self, req, event_pk, format=None):
+        """ Return participation entity for event: account link + parts """
         e = get_event(event_pk)
         ps = ParticipationSerializer(e.get_participants(), many=True,
                                      context={'request': req})
         return Response(ps.data)
 
     def post(self, req, event_pk, format=None):
-        """
-        Expect array, like: [{"rate": 1, "account": 1}, ...]. Account contains
-        pk.
+        """ Add participant to event in context.
+        Expect array, like: [{"rate": 1, "account": 1}, ...]. account is pk.
         """
         ser = ParticipationPostSerializer(data=req.data,
                                           context={'request': req}, many=True)
@@ -85,7 +120,9 @@ class ParticipantListView(views.APIView):
 
 
 class ParticipantDetail(views.APIView):
+    """ Single Instance view for participation entity (accout + parts). """
     def get(self, req, event_pk, pk):
+        """ Show concrete participation details. """
         e = get_event(event_pk)
         p = get_participation(e, pk)
 
@@ -94,6 +131,7 @@ class ParticipantDetail(views.APIView):
         )
 
     def delete(self, req, event_pk, pk):
+        """ Remove participant from event. """
         e = get_event(event_pk)
         p = get_participation(e, pk)
         e.remove_participants([p['account']])
