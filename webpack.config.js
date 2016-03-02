3eafe8d73d243892b53b12f113e108265d20e55e
Intey <ziexe0@gmail.com>
2016-03-02 23:09:15 +0300

    [fix] Configs: use 300 port for dev server, many entry points of JS.

--- webpack.config.js
+++ webpack.config.js
@@ -5,20 +5,25 @@ var BundleTracker = require('webpack-bundle-tracker')
 module.exports = {
     context: __dirname,
 
-    entry: [
-		'webpack-dev-server/client?http://localhost:3000',
-		'webpack/hot/only-dev-server',
-        './assets/js/index', // entry point
-	],
+    entry: {
+        main: './assets/js/index.js',
+        events: './assets/js/events.js',
+        users: './assets/js/users.js',
+        auth: './assets/js/auth.js',
+    },
     output: {
         path: path.resolve('./assets/bundles/'),
-        filename: '[name]-[hash].js',
+        filename: '[name].js', // use entry field name.
+        // for hot reload. Does it need?
         publicPath: 'http://localhost:3000/assets/bundles/',
     },
 
     plugins: [
+        // reload only changed part of page
         new webpack.HotModuleReplacementPlugin(),
+        // no genereta empty output, if errors occur
         new webpack.NoErrorsPlugin(),
+        // integration with django
         new BundleTracker({filename: './webpack-stats.json'}),
     ],
 
