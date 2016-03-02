020a0c00eac8a33375447b8891cb901f5d791997
Intey <ziexe0@gmail.com>
2016-03-02 22:37:48 +0300

    [fix] Remove study data.

--- assets/js/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-// import Field from './field.jsx';
-
-export class App extends React.Component{
-   render() {
-       return <div> Hey bitch </div>;
-   }
-};
--- assets/js/field.jsx
+++
@@ -1,7 +0,0 @@
-// import React from 'react';
-//
-// class Field extends React.Component {
-//     render() {
-//         return (<span>Holly shit</span>);
-//     }
-// }
