292254faafdead822fdabe6920701747654d992f
Intey <ziexe0@gmail.com>
2016-03-10 23:12:12 +0300

    [fix] Show events table.

--- banking/frontend/js/eventtable.js
+++ banking/frontend/js/eventtable.js
@@ -9,36 +9,19 @@ module.exports = React.createClass({
             return (<EventRow key={idx} data={event}/>);
         });
         return (
-            <div className="col-md-12">
-                <h3>Список событий</h3>
-                <div className="row">
-                    <div className="col-md-10">
-                    </div>
-                    <div className="col-md-2">
-                        <a className="btn btn-success" href="#" style={{marginTop:'16px'}} data-toggle="modal" data-target="#new-event-dlg">
-                            <span className="glyphicon glyphicon-plus"></span> Новое событие
-                        </a>
-                    </div>
-                </div>
-                <div className="row" style={{marginTop:'20px'}}>
-                    <div className="col-md-12">
-                        <table className="table" id="event-table">
-                            <thead>
-                                <tr>
-                                    <th style={{width:'100px'}}></th>
-                                    <th>Название</th>
-                                    <th>Дата</th>
-                                    <th>Сумма</th>
-                                    <th>Владелец</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {events}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+            <table className="table" id="event-table">
+                <thead>
+                    <tr>
+                        <th>Название</th>
+                        <th>Дата</th>
+                        <th>Сумма</th>
+                        <th>Владелец</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {events}
+                </tbody>
+            </table>
         );
     }
 });
@@ -47,11 +30,10 @@ var EventRow = React.createClass({
     render: function(){
         return (
             <tr>
-                <th>No data</th>
-                <th>No data</th>
-                <th>No data</th>
-                <th>No data</th>
-                <th>No data</th>
+                <td><a href={"/events/"+this.props.data.id}>{this.props.data.name}</a></td>
+                <td>{this.props.data.date}</td>
+                <td>{this.props.data.price}</td>
+                <td>{this.props.data.author}</td>
             </tr>
         )}
 });
