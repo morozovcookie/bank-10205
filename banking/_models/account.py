20d49a0a99b785cd3a3fd70ba50a33f2722513c9
Intey <ziexe0@gmail.com>
2016-03-26 17:35:20 +0300

    [ref] User 'pay' to event on participation

--- banking/_models/account.py
+++ banking/_models/account.py
@@ -4,8 +4,6 @@ from django.db import models
 from django.db.models import F, Sum
 from django.contrib.auth.models import User
 
-from banking.operations.domain.event import sawn
-
 
 class Account(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -15,21 +13,9 @@ class Account(models.Model):
         """Account push money to bank. Distribute count on debted events (older
         event filled firstly."""
         from .transfer import Transfer
-        from .transaction import Transaction
 
         t = Transfer(account=self, direction="IN", count=count)
         t.save()
-        # get events with positive balance. Substract debit from credit, 'couze
-        # we need event balance. Event 'health' is credit(account fill this
-        # with participation). Debit - is damage(account pay to event).
-        events = [tt.event for tt in
-                  Transaction.objects.filter(account=self)
-                  .annotate(unpayed=F('credit') - F('debit'))
-                  .filter(unpayed__gt=0)
-                  .values('event')
-                  .order_by('event__date').distinct()]
-
-        sawn(count, self, events)
 
     def out_money(self, count):
         """account call to get money from the bank. Make every effort that he did
--- banking/operations/domain/event.py
+++ banking/operations/domain/event.py
@@ -1,25 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-def sawn(money, user, events):
-    """Split money between events."""
-    from banking.models import Transaction
-    raise NotImplementedError("sawn income money between user debted events")
-
-    for e in events:
-        if money == 0:
-            break
-
-        rest = e.rest()
-        # if e.rest > 0, fill
-        t = Transaction(user=user, event=e,
-                        debit=money - (money - rest), rate=1)
-        t.save()
-        money -= rest
-
-        e.save()
-
-
 def get_participants(event):
     """Get participants of Event
     @return:  participants List of dicts, where keys: 'account', 'parts'.
