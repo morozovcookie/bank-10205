daf42f8dd8bcd2a6adaa6a968bec5e66d23a0403
Intey <ziexe0@gmail.com>
2016-04-03 18:50:55 +0300

    [fix] webpack Config for dev & prod.

--- webpack.config.js
+++ webpack.config.js
@@ -2,68 +2,92 @@ var path = require("path")
 var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 
-module.exports = {
-    context: path.resolve(__dirname, './banking/'),
+// yo dawg i heard you like configs so we put a config in yo config so you can
+// config when you config.
+const DEFS = JSON.parse(process.env.BANK);
 
-    entry: {
-        //
-        index:  ['./frontend/js/index.js'],
-        summary:['./frontend/js/summary.js'], // index table. Events, participants...
-        events: ['./frontend/js/events.js'],
-        users:  ['./frontend/js/users.js'],
-        auth:   ['./frontend/js/auth.js'],
-        jquery: ['../node_modules/jquery/dist/jquery.min.js'],
-        bootstrap_js: ['../node_modules/bootstrap/dist/js/bootstrap.min.js'],
-    },
-    output: {
-        path: path.resolve(__dirname, './banking/static/js'),
-        filename: '[name].js', // use entry field name.
-        // for hot reload.
-        publicPath: '/static/js/',
-        library: '$' // for inlined JS in HTML.
-    },
+var config = {
+context: path.resolve(__dirname, './banking/'),
+
+entry: {
+    //
+    index:  ['./frontend/js/index.js'],
+    summary:['./frontend/js/summary.js'], // index table. Events, participants...
+    events: ['./frontend/js/events.js'],
+    users:  ['./frontend/js/users.js'],
+    auth:   ['./frontend/js/auth.js'],
+    jquery: ['../node_modules/jquery/dist/jquery.min.js'],
+    bootstrap_js: ['../node_modules/bootstrap/dist/js/bootstrap.min.js'],
+},
+output: {
+    path: path.resolve(__dirname, './banking/static/js'),
+    filename: '[name].js', // use entry field name.
+    // for hot reload.
+    publicPath: '/static/js/',
+    library: '$' // for inlined JS in HTML.
+},
 
 
-    plugins: [
-        // reload only changed part of page
-        // new webpack.HotModuleReplacementPlugin(),
-        // no genereta empty output, if errors occur
-        new webpack.NoErrorsPlugin(),
-        // integration with django
-        new BundleTracker({filename: "./webpack-prod-stats.json"}),
-        // new webpack.optimize.CommonsChunkPlugin({
-        //     name: 'commons',
-        // }),
+plugins: [
+    // no genereta empty output, if errors occur
+    new webpack.NoErrorsPlugin(),
+    // integration with django
+    new BundleTracker({filename: "./webpack-prod-stats.json"}),
+    // for bootstrap.js in node_modules
+    new webpack.ProvidePlugin({ jQuery: 'jquery', }),
+],
 
-        new webpack.ProvidePlugin({ // for bootstrap.js in node_modules
-            jQuery: 'jquery',
-        }),
-    ],
+module: {
+    loaders: [
+        // to transform JSX into JS
+        {
+            test: [/\.js?$/, /\.jsx?$/],
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        },
+    ]
+},
 
-    module: {
-        loaders: [
-            // to transform JSX into JS
-            {
-                test: [/\.js?$/, /\.jsx?$/],
-                exclude: /node_modules/,
-                loader: 'react-hot',
-            },
-            {
-                test: [/\.js?$/, /\.jsx?$/],
-                exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
-            },
-        ]
-    },
+resolve: {
+    root: __dirname,
+    modulesDirectories: ['node_modules', 'bower_components'],
+    extensions: ['', '.js', '.jsx']
+},
 
-    resolve: {
-        root: __dirname,
-        modulesDirectories: ['node_modules', 'bower_components'],
-        extensions: ['', '.js', '.jsx']
-    },
-
-    //  devtool: 'eval',
+//  devtool: 'eval',
 }
+
+var plugins = [];
+var loaders = [];
+
+// diff between dev & prod.
+if (DEFS.dev) {
+    plugins.push(
+        new webpack.HotModuleReplacementPlugin()
+    );
+
+    loaders.push(
+        { // hope that order not does not affect
+            test: [/\.js?$/, /\.jsx?$/],
+            exclude: /node_modules/,
+            loader: 'react-hot'
+        }
+    );
+}
+else {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false },
+            // prevent renaming(mangling) this vars
+            mangle: { except: ['$super', '$', 'exports', 'require'] }
+        })
+    );
+}
+
+config.plugins = config.plugins.concat(plugins);
+config.module.loaders = config.module.loaders.concat(loaders);
+
+module.exports = config;
