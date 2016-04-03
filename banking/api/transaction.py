133e45b3a3f4020651170a9a2816f259ca93a376
Intey <ziexe0@gmail.com>
2016-04-03 22:47:21 +0300

    [up] Improve transactions api filters.

--- banking/api/transaction.py
+++ banking/api/transaction.py
@@ -1,19 +1,28 @@
-# from django.http import Http404
-# from rest_framework.response import Response
+import django_filters
+
 from rest_framework import generics
 
 from rest_framework import filters
 # import django_filters  # for fields
 
-from banking.models import Transaction
+from banking.models import Transaction, Event, Account
 from banking.serializers.transaction import TransactionSerializer
 
 
 class TransactionFilter(filters.FilterSet):
+    participants = django_filters.ModelMultipleChoiceFilter(
+        name='participation__account',
+        queryset=Account.objects.all())
+    event = django_filters.ModelChoiceFilter(name='participation__event',
+                                             queryset=Event.objects.all())
+    parts = django_filters.NumberFilter(name='participation__parts')
+    type = django_filters.MultipleChoiceFilter(name='TYPES')
+
     class Meta:
         model = Transaction
-        fields = ['date', 'credit', 'debit', 'type', ]
-        # 'parts', 'account', 'event'
+        queryset = Transaction.objects.all()
+        fields = ['date', 'credit', 'debit', 'type', 'participants', 'event',
+                  'parts']
 
 
 class TransactionListView(generics.ListAPIView):
