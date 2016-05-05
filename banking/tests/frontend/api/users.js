1280945b28767190e4d381f8f8a4e4b24762b54f
Intey <ziexe0@gmail.com>
2016-05-05 13:24:30 +0300

    [ref][tests] Remove semicolons.

--- banking/tests/frontend/api/users.js
+++ banking/tests/frontend/api/users.js
@@ -1,45 +1,46 @@
 import jsdom, {rerequire} from 'mocha-jsdom'
 import sinon from 'sinon'
-import {expect, assert} from 'chai';
-var should = require('chai').should(); // actually call the function
+import {expect, assert} from 'chai'
+var should = require('chai').should() // actually call the function
 
-import modules from './defines.js';
+import modules from './defines.js'
 
-const userListPath = require(modules.endpoints).EndPoint.UserList();
+const EndPoint = require(modules.endpoints).EndPoint
+const userListPath = EndPoint.UserList()
 
 describe("When call API", function() {
-    var $;
-    jsdom();
+    var $
+    jsdom()
 
     describe("create user", function() {
         beforeEach(function() {
-            $ = require('jquery');
-            sinon.stub($, 'ajax');
-            this.API = require(modules.api).AccountAPI;
-        });
+            $ = require('jquery')
+            sinon.stub($, 'ajax')
+            this.API = require(modules.api).AccountAPI
+        })
 
         it(`should POST to ${userListPath}`, function() {
-            var userdata = {username: "test", password: "test"};
-            this.API.createAccount(userdata);
+            var userdata = {username: "test", password: "test"}
+            this.API.createAccount(userdata)
 
-            var p = $.ajax.getCall(0).args[0];
-            assert.equal(p.data, userdata);
-            assert.equal(p.url, userListPath);
+            var p = $.ajax.getCall(0).args[0]
+            assert.equal(p.data, userdata)
+            assert.equal(p.url, userListPath)
 
-            userdata = {username: "Bobby", password: "123"};
+            userdata = {username: "Bobby", password: "123"}
 
-            $.ajax.reset();
+            $.ajax.reset()
 
-            this.API.createAccount(userdata);
+            this.API.createAccount(userdata)
 
-            var p = $.ajax.getCall(0).args[0];
-            assert.equal(p.data, userdata);
-            assert.equal(p.url, userListPath);
-        });
+            var p = $.ajax.getCall(0).args[0]
+            assert.equal(p.data, userdata)
+            assert.equal(p.url, userListPath)
+        })
 
-        afterEach(function() { $.ajax.restore(); });
-    });
+        afterEach(function() { $.ajax.restore() })
+    })
 
 
-});
+})
 
