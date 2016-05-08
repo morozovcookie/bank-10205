5c2e6eba731ee56afee1d685c38edcd0bd19074e
Intey <ziexe0@gmail.com>
2016-05-09 00:31:00 +0300

    [ref][front] Rename transactions.js to participationEditList.js

---
+++ banking/frontend/js/participantsEditList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Transactions from './components/transactions'
+
+ReactDOM.render(
+    <Transactions/>,
+    document.getElementById('transactions')
+);
