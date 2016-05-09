f2d2c3ea035c6100d5f44ecb9a7c4576e8b691c5
Intey <ziexe0@gmail.com>
2016-05-09 09:55:10 +0300

    [ref][front] Rename participants edit list.

---
+++ banking/frontend/js/participants.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ParticipantEditList from './components/participantsEditList'
+
+ReactDOM.render(
+    <ParticipantEditList/>,
+    document.getElementById('participants')
+);
