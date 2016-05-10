40f5f87dc6d0b3bd4c7d39bd127e6461dc821d1c
Intey <ziexe0@gmail.com>
2016-05-11 00:54:12 +0300

    [clean][debug] remove debug prints

--- banking/operations/domain/event.py
+++ banking/operations/domain/event.py
@@ -95,16 +95,13 @@ def add_participants(event, newbies):
     parent_transactions = []
     # participate incomers
     for (acc, parts) in newbies.items():
-        print("ADDING", acc)
         # if not already participated
         participation = Participation.objects.filter(account=acc, event=event,
                                                      active=False)
         if len(participation) == 0:
-            print("NOT participated yet")
             participation = Participation(account=acc, parts=parts,
                                           event=event)
         else:
-            print("ALREADY participated", participation)
             participation = participation[0]
 
         participation.active = True
@@ -114,7 +111,6 @@ def add_participants(event, newbies):
                          type=Transaction.PARTICIPATE)
         tr.credit = party_pay * parts
         parent_transactions.append(tr)
-        print("CREATE TRANSACTION", tr)
         tr.save()
 
     # create diffs for old participants
