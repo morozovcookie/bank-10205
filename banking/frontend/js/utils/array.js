03a0b10bc72e6cf498f388a350ec6fd22a6b3dd7
Intey <ziexe0@gmail.com>
2016-05-11 00:54:48 +0300

    [up][front] IntArray intersection.

---
+++ banking/frontend/js/utils/array.js
@@ -0,0 +1,17 @@
+export function intersect(a, b) {
+	var ai=0, bi=0
+	var result = []
+
+	while( ai < a.length && bi < b.length )
+		{
+			if      (a[ai] < b[bi] ){ ai++ }
+			else if (a[ai] > b[bi] ){ bi++ }
+			else /* they're equal */
+			{
+				result.push(a[ai])
+				ai++
+				bi++
+			}
+		}
+		return result
+}
