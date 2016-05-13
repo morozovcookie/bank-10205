c9f3f876f726c1b8e928e54f6fca130ca2b03de9
Intey <ziexe0@gmail.com>
2016-05-13 11:12:42 +0000

    [fix][tests] DRY.

--- banking/tests/backend/api/auth_test.py
+++ banking/tests/backend/api/auth_test.py
@@ -10,45 +10,55 @@ class AuthenticationTest(TestCase):
     def setUp(self):
         self.factory = APIClient()
 
-    def test_register(self):
-        response = self.factory.post('/api/users/',
-                                     {
-                                         'username': 'TestUser',
-                                         'password': 'raw',
-                                         'first_name': 'Test',
-                                         'last_name': 'User',
-                                         'is_superuser': False,
-                                     })
+        # fixtures
 
+        self.reg_data = {
+            'username': 'TestUser',
+            'password': 'raw',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'is_superuser': False,
+        }
+        self.username = 'Registred'
+        self.password = 'password'
+        # we need registered user in tests
+        self.register({
+            'username': self.username,
+            'password': self.password,
+            'first_name': 'Test',
+            'last_name': 'User',
+            'is_superuser': False,
+        })
+
+    def register(self, udata):
+        """Register user and return response."""
+        response = self.factory.post('/api/users/', udata)
+        return response
+
+    def register_test(self):
+        response = self.register(self.reg_data)
         self.assertEqual(response.status_code, status.HTTP_200_OK,
-                         msg=response.content)
+                         msg=response.content.decode('utf-8'))
 
-    def test_wrong_credentials(self):
+    def wrong_credentials_test(self):
         response = self.factory.post('/api/auth/', {'username': 'SomeName',
                                                     'password': 'raw'})
-
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST,
-                         msg=response.content)
+                         msg=response.content.decode('utf-8'))
 
-    def test_auth_created_user(self):
-        username = 'TestUser'
-        raw_password = 'raw'
-        response = self.factory.post('/api/users/',
-                                     {
-                                         'username': username,
-                                         'password': raw_password,
-                                         'first_name': 'Test',
-                                         'last_name': 'User',
-                                         'is_superuser': False,
-                                     })
-
-        data = response.content.decode('utf-8')
+    def auth_created_user_test(self):
+        response = self.factory.post('/api/auth/',
+                                     {'username': self.username,
+                                      'password': self.password})
 
         self.assertEqual(response.status_code, status.HTTP_200_OK,
-                         msg=data)
+                         msg=response.content.decode('utf-8'))
 
-        response = self.factory.post('/api/auth/', {'username': username,
-                                                    'password': raw_password})
+    def auth_response_test(self):
+        pass
+        response = self.factory.post('/api/auth/',
+                                     {'username': self.username,
+                                      'password': self.password})
 
         self.assertEqual(response.status_code, status.HTTP_200_OK,
                          msg=response.content.decode('utf-8'))
